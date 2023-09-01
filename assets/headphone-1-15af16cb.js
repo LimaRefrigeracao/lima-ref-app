c88d1257a2d1c019f9fa73b46e5866f304a774e3
JoaoPedroSH <84398388+JoaoPedroSH@users.noreply.github.com>
2023-09-01 06:03:53 -0300

    Updates

---
+++ assets/headphone-1-15af16cb.js
@@ -0,0 +1,1 @@
+const s="/refis-app/images/faces/2.jpg",p="/refis-app/images/products/headphone-1.jpg";export{s as _,p as a};
---
+++ assets/index-82e01bda.js
@@ -0,0 +1,1 @@
+import{_ as o,v as r,b as n,B as t}from"./index-15e5b3fd.js";const c={};function s(_,a){const e=r("router-view");return n(),t(e)}const i=o(c,[["render",s]]);export{i as default};
