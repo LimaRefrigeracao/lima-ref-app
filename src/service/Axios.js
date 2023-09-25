aa5547d3e00cc2a8b803fbc310c2a4e8cdd4a3cd
JoaoPedroSH <84398388+JoaoPedroSH@users.noreply.github.com>
2023-09-25 07:00:48 -0300

    Update Axios.js

--- src/service/Axios.js
+++ src/service/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosInstance = () => {
     const token = localStorage.getItem('token');
     return axios.create({
-        baseURL: 'http://localhost:3333/',
+        baseURL: import.meta.env.VITE_BASE_URL_API,
         headers: {
             Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json'
