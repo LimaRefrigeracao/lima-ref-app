cc7c603c8fc03b6cee519ad894979ff2cf4b07a4
JoaoPedroSH <84398388+JoaoPedroSH@users.noreply.github.com>
2023-10-14 13:17:31 -0300

    Update CardInfoServiceByProducts.vue

--- src/views/operational/ControlPanel/components/CardInfoServiceByProducts.vue
+++ src/views/operational/ControlPanel/components/CardInfoServiceByProducts.vue
@@ -1,78 +1,45 @@
 <script setup>
 import { onMounted, ref } from 'vue';
+import Axios from '../../../../service/Axios';
+import { loadingOpen, loadingClose } from '../../../components/computeds.js';
+import { useToast } from 'primevue/usetoast';
+const toast = useToast();
 
-onMounted(() => {});
+const data = ref([]);
+const getCountProductByService = async () => {
+    loadingOpen();
+    try {
+        const response = await Axios.get('/panel_control/product_by_service');
+        data.value = response.data;
+        console.log(response.data)
+        loadingClose();
+    } catch (error) {
+        toast.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao buscar dados', life: 5000 });
+        console.error(error);
+        loadingClose();
+    }
+};
+
+onMounted(() => {
+    getCountProductByService();
+});
 </script>
 <template>
+    <Toast />
     <div class="card">
         <div class="flex justify-content-between align-items-center mb-5">
             <h5>Quantidade por Tipo de Produto</h5>
         </div>
         <ul class="list-none p-0 m-0">
-            <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
+            <li v-for="product in data.values" :key="product.index" class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
                 <div>
-                    <span class="text-900 mr-2 mb-1 md:mb-0">Space T-Shirt</span>
+                    <span class="text-900 mr-2 mb-1 md:mb-0">{{ product.name }}</span>
                 </div>
                 <div class="mt-2 md:mt-0 flex align-items-center">
                     <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                        <div class="bg-orange-500 h-full" style="width: 50%"></div>
+                        <div :class="'bg-' + product.color + ' h-full'" :style="'width:' + Math.floor(product.count / data.length * 100) + '%'"></div>
                     </div>
-                    <span class="text-orange-500 ml-3 font-medium">%50</span>
-                </div>
-            </li>
-            <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                <div>
-                    <span class="text-900 mr-2 mb-1 md:mb-0">Portal Sticker</span>
-                </div>
-                <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                    <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                        <div class="bg-cyan-500 h-full" style="width: 16%"></div>
-                    </div>
-                    <span class="text-cyan-500 ml-3 font-medium">%16</span>
-                </div>
-            </li>
-            <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                <div>
-                    <span class="text-900 mr-2 mb-1 md:mb-0">Supernova Sticker</span>
-                </div>
-                <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                    <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                        <div class="bg-pink-500 h-full" style="width: 67%"></div>
-                    </div>
-                    <span class="text-pink-500 ml-3 font-medium">%67</span>
-                </div>
-            </li>
-            <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                <div>
-                    <span class="text-900 mr-2 mb-1 md:mb-0">Wonders Notebook</span>
-                </div>
-                <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                    <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                        <div class="bg-green-500 h-full" style="width: 35%"></div>
-                    </div>
-                    <span class="text-green-500 ml-3 font-medium">%35</span>
-                </div>
-            </li>
-            <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                <div>
-                    <span class="text-900 mr-2 mb-1 md:mb-0">Mat Black Case</span>
-                </div>
-                <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                    <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                        <div class="bg-purple-500 h-full" style="width: 75%"></div>
-                    </div>
-                    <span class="text-purple-500 ml-3 font-medium">%75</span>
-                </div>
-            </li>
-            <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                <div>
-                    <span class="text-900 mr-2 mb-1 md:mb-0">Robots T-Shirt</span>
-                </div>
-                <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                    <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                        <div class="bg-teal-500 h-full" style="width: 40%"></div>
-                    </div>
-                    <span class="text-teal-500 ml-3 font-medium">%40</span>
+                    <span :class="'text-' + product.color + ' ml-3 font-medium'">{{ product.count }}</span>
                 </div>
             </li>
         </ul>
