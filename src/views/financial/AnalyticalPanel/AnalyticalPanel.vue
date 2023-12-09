0cb56449fc7745981084a6371a726a3d9e26ced3
JoaoPedroSH <84398388+JoaoPedroSH@users.noreply.github.com>
2023-12-09 03:04:30 -0300

    update

--- src/views/financial/AnalyticalPanel/AnalyticalPanel.vue
+++ src/views/financial/AnalyticalPanel/AnalyticalPanel.vue
@@ -1,43 +1,13 @@
 <script setup>
-import { ref } from 'vue';
-import CardsNetEarnings from './components/CardsNetEarnings.vue';
-import CardsGrossEarnings from './components/CardsGrossEarnings.vue';
-const maxDate = ref(new Date());
-const dates = ref();
-const date = ref(new Date());
-const type = ref('Bruto');
-const options = ref(['Bruto', 'Líquido']);
-const selectedTypeSearch = ref({ name: 'Geral', code: 'geral' });
-const typeSearch = ref([
-    { name: 'Geral', code: 'geral' },
-    { name: 'Período de Tempo', code: 'periodo' },
-    { name: 'Por Mês', code: 'mes' },
-    { name: 'Por Ano', code: 'ano' }
-]);
+import CardsEarnings from './components/CardsEarnings.vue';
 </script>
 
 <template>
     <div class="grid flex justify-content-center">
         <div class="col-12 xl:col-12">
             <div class="card">
-                <h5>MOVIMENTAÇÃO</h5>
-                <Toolbar class="mb-4">
-                    <template v-slot:start>
-                        <div >
-                            <Dropdown v-model="selectedTypeSearch" editable :options="typeSearch" optionLabel="name" placeholder="Select a City" class="ml-2 w-full md:w-13rem" />
-                            <Calendar class="ml-2" v-if="selectedTypeSearch.code == 'periodo'" v-model="dates" selectionMode="multiple" />
-                            <Calendar class="ml-2" v-else-if="selectedTypeSearch.code == 'mes'" v-model="date" view="month" dateFormat="mm/yy" :maxDate="maxDate" />
-                            <Calendar class="ml-2" v-else-if="selectedTypeSearch.code == 'ano'" v-model="date" view="year" dateFormat="yy" :maxDate="maxDate" />
-                            <Button class="ml-2" icon="pi pi-search" v-tooltip.top="'Pesquisar'" />
-                        </div>
-                    </template>
-                    <template v-slot:end>
-                        <SelectButton v-model="type" :options="options" aria-labelledby="basic" />
-                    </template>
-                </Toolbar>
-
-                <CardsGrossEarnings v-if="type == 'Bruto'" />
-                <CardsNetEarnings v-else />
+                <h5 class="mb-4" style="color:#16A704;">ENTRADAS <i class="pi pi-sign-in" style="font-size: 16px"></i></h5>
+                <CardsEarnings />
             </div>
         </div>
     </div>
---
+++ src/views/financial/AnalyticalPanel/components/CardsEarnings.vue
@@ -0,0 +1,120 @@
+<script setup>
+import { ref, onBeforeMount, Axios, loadingOpen, loadingClose, useToast } from '@/views/common';
+const toast = useToast();
+
+const dataEarnings = ref({
+    daily: {
+        value: 0,
+        day: ''
+    },
+    weekly: {
+        value: 0,
+        week: ''
+    },
+    monthly: {
+        value: 0,
+        month: ''
+    },
+    yearly: {
+        value: 0,
+        year: ''
+    }
+});
+const getInfoEarnings = async () => {
+    loadingOpen();
+    try {
+        const response = await Axios.get('/panel_analytical/info_values_os_paid');
+        dataEarnings.value = response.data;
+        console.log(dataEarnings.value);
+        loadingClose();
+    } catch (error) {
+        toast.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao buscar informações das entrada!', life: 5000 });
+        console.error(error);
+        loadingClose();
+    }
+};
+
+onBeforeMount(() => {
+    getInfoEarnings();
+});
+</script>
+<template>
+    <div class="grid">
+        <div class="col-12 lg:col-6 xl:col-3">
+            <Card>
+                <template #header>
+                    <Toolbar>
+                        <template #start>
+                            <span class="block text-700 font-medium mb-2 mt-2">DIÁRIO</span>
+                        </template>
+                        <template #end>
+                            <span class="text-green-500 font-medium"> {{ dataEarnings.daily.day }} </span>
+                        </template>
+                    </Toolbar>
+                </template>
+                <template #content>
+                    <div class="flex justify-content-center align-items-center">
+                        <div class="text-900 font-medium text-xl">R$ {{ dataEarnings.daily.value.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}</div>
+                    </div>
+                </template>
+            </Card>
+        </div>
+        <div class="col-12 lg:col-6 xl:col-3">
+            <Card>
+                <template #header>
+                    <Toolbar>
+                        <template #start>
+                            <span class="block text-700 font-medium mb-2 mt-2">SEMANAL</span>
+                        </template>
+                        <template #end>
+                            <span class="text-green-500 font-medium"> {{ dataEarnings.weekly.week }}</span>
+                        </template>
+                    </Toolbar>
+                </template>
+                <template #content>
+                    <div class="flex justify-content-center align-items-center">
+                        <div class="text-900 font-medium text-xl">R$ {{ dataEarnings.weekly.value.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}</div>
+                    </div>
+                </template>
+            </Card>
+        </div>
+        <div class="col-12 lg:col-6 xl:col-3">
+            <Card>
+                <template #header>
+                    <Toolbar>
+                        <template #start>
+                            <span class="block text-700 font-medium mb-2 mt-2">MENSAL</span>
+                        </template>
+                        <template #end>
+                            <span class="text-green-500 font-medium"> {{ dataEarnings.monthly.month }} </span>
+                        </template>
+                    </Toolbar>
+                </template>
+                <template #content>
+                    <div class="flex justify-content-center align-items-center">
+                        <div class="text-900 font-medium text-xl">R$ {{ dataEarnings.monthly.value.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}</div>
+                    </div>
+                </template>
+            </Card>
+        </div>
+        <div class="col-12 lg:col-6 xl:col-3">
+            <Card>
+                <template #header>
+                    <Toolbar>
+                        <template #start>
+                            <span class="block text-700 font-medium mb-2 mt-2">ANUAL</span>
+                        </template>
+                        <template #end>
+                            <span class="text-green-500 font-medium"> {{ dataEarnings.yearly.year }} </span>
+                        </template>
+                    </Toolbar>
+                </template>
+                <template #content>
+                    <div class="flex justify-content-center align-items-center">
+                        <div class="text-900 font-medium text-xl">R$ {{ dataEarnings.yearly.value.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}</div>
+                    </div>
+                </template>
+            </Card>
+        </div>
+    </div>
+</template>
